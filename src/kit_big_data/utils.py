3ae8463b1ae9efdc77f01c1d14a17c84c0ab7927
nohaila.garra <nohaila.garra@yosobox.com>
2024-12-08 15:14:46 +0100

    Initial commit for Kit_Big_Data

---
+++ src/kit_big_data/utils.py
@@ -0,0 +1,9 @@
+from logging_config import setup_logging
+
+logger = setup_logging()
+
+def log_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        # Ne pas logger les interruptions clavier
+        return
+    logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
